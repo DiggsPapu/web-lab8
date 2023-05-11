715d1c25641084fa87bb68cd0776f896e692f70e
DieggsPapu <alo20172@uvg.edu.gt>
2023-05-11 17:08:43 -0600

    Config form is in one hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter, RouterProvider, Routes, Route,
-} from 'react-router-dom'
 import './index.css'
 import App from './App'
 
